ea3b67793fe25be323793235b62906ab19622e24
Zhile Lin <zhlin04@gmail.com>
2023-04-11 20:05:24 -0700

    test

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const app = express()
 dotenv.config()
 
 
-app.use(express.static('frontend/build'));
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+     res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
    })
 
 const connect= () =>{
