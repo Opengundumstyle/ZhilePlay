168ecbca4a36ff5a4a85aa15237045126873b75d
Zhile Lin <zhlin04@gmail.com>
2023-04-11 19:52:44 -0700

    test

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ dotenv.config()
 
 app.use(express.static('client/build'));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
    })
-
 
 const connect= () =>{
      mongoose.connect(process.env.MONGODB_API_KEY).then(()=>{
